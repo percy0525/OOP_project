3d7993eb503e10ab327603211c7b5ada604d9ffb
Jay <a121406@gmail.com>
2019-04-16 16:07:10 +0800

    0416 commit

--- src/gui/UMLCanvas.java
+++ src/gui/UMLCanvas.java
@@ -9,26 +9,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.*;
 import java.util.ArrayList;
 
 
-public class UMLCanvas extends JPanel implements MouseListener {
+public class UMLCanvas extends JPanel implements MouseListener, MouseMotionListener {
     public static String mode = "";
     private final int W = 100;
     private final int H = 200;
     private ArrayList<Shape> objContainer = new ArrayList<Shape>();
     private ArrayList<Port> portContainer = new ArrayList<Port>();
+    private Point mousePress;
+    private Point mouseMove;
+    private Point mouseRelease;
     public UMLCanvas(){
         super();
         this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+        this.mousePress = new Point();
+        this.mouseMove = new Point();
+        this.mouseRelease = new Point();
     }
-
 
     @Override
-    public void mouseClicked(MouseEvent e) {
-//
-    }
+    public void mouseClicked(MouseEvent e) { }
 
     @Override
     public void mousePressed(MouseEvent e) {
@@ -36,52 +41,73 @@ public class UMLCanvas extends JPanel implements MouseListener {
             Graphics g = this.getGraphics();
             ClassObject class_object = new ClassObject(e.getPoint(), new Dimension(W,H), "new class", false);
             objContainer.add(class_object);
-            for (Shape object : objContainer){
-                object.paint(g);
-                //System.out.println("obj location:" + object.xy);
-            }
+            this.repaint();
         }
         else if (mode == "select"){
-
+            this.mousePress.setLocation(e.getPoint());
+            this.mouseMove.setLocation(e.getPoint());
             /* TODO replace dimension */
             checkObject(objContainer, e.getPoint(), new Dimension(W,H));
-            for (Shape object : objContainer){
-                //System.out.println(object.isSelected);
-                if(object.isSelected){
-                    Graphics g = this.getGraphics();
-                    Port portObj = new Port(e.getPoint());
-//                    portContainer.add(portObj);
-                    portObj.paint(object, g);
-                }
-                else{
-                    Graphics g = this.getGraphics();
-                    Port portObj = new Port(e.getPoint());
-                    portObj.clear(object, g);
-                }
-                Graphics g = this.getGraphics();
-                object.paint(g);
-            }
+            this.repaint();
         }
         else if(mode == "use_case"){
-            Graphics g = this.getGraphics();
+            //Graphics g = this.getGraphics();
             UseCaseObject use_case_object = new UseCaseObject("use_case", e.getPoint(), new Dimension(H,W), false);
             objContainer.add(use_case_object);
-            for (Shape object : objContainer){
-                object.paint(g);
-                //System.out.println("obj location:" + object.xy);
-            }
+            this.repaint();
         }
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {    }
+    public void mouseReleased(MouseEvent e) {
+        this.mouseRelease.setLocation(e.getPoint());
+
+        for(Shape object : objContainer) {
+            if(object.xy.getX() > this.mousePress.getX() && object.xy.getY() >  this.mousePress.getY()
+                    && object.xy.getX() < this.mouseRelease.getX() && object.xy.getY() < this.mouseRelease.getY()) {
+                object.isSelected = true;
+            }
+        }
+
+        this.repaint();
+    }
 
     @Override
     public void mouseEntered(MouseEvent e) {    }
-
     @Override
     public void mouseExited(MouseEvent e) {    }
 
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        //System.out.println(this.mouseMove);
+        if (mode == "select") {
+            int offsetX = e.getX() - (int) this.mouseMove.getX();
+            int offsetY = e.getY() - (int) this.mouseMove.getY();
+            this.mouseMove.setLocation(e.getPoint());
+            checkObject(objContainer, e.getPoint(), new Dimension(W, H));
+
+            for (Shape object : this.objContainer) {
+                if (object.isSelected) {
+                    object.translate(offsetX, offsetY);
+                }
+            }
+            this.repaint();
+        }
+    }
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        for(Shape shape : objContainer) {
+            shape.paint(g);
+            if (shape.isSelected){
+                shape.paintPort(g);
+            }
+        }
+    }
     private void checkObject(ArrayList<Shape> objContainer, Point now, Dimension wh){
         //boolean hasObject = false;
         for (Shape object : objContainer){
--- src/shape/Shape.java
+++ src/shape/Shape.java
@@ -6,8 +6,19 @@ public class Shape {
     public String name;
     public Point xy;
     private final int NUM_OF_PORT = 4;
-    protected Port [] portArray;
     public boolean isSelected;
+    private final int PORT_WIDTH = 10;
+    private final int PORT_HEIGHT = 10;
 
     public void paint(Graphics g){}
+    public void translate(int dx, int dy) {
+        this.xy.setLocation(xy.getX() + dx, xy.getY() + dy);
+    }
+    public void paintPort(Graphics g){
+        g.fillRect((int)this.xy.getX() + (int)(this.widthHeight.getWidth()/2), (int)this.xy.getY(), PORT_WIDTH, PORT_HEIGHT);
+        g.fillRect((int)this.xy.getX(), (int)(this.xy.getY() + this.widthHeight.getHeight()/2), PORT_WIDTH, PORT_HEIGHT);
+        g.fillRect((int)(this.xy.getX() + this.widthHeight.getWidth()/2), (int)(this.xy.getY() + this.widthHeight.getHeight()), PORT_WIDTH, PORT_HEIGHT);
+        g.fillRect((int)this.xy.getX() + (int)(this.widthHeight.getWidth()), (int)(this.xy.getY() + this.widthHeight.getHeight()/2), PORT_WIDTH, PORT_HEIGHT);
+    }
 }
+
