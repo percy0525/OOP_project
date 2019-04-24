eb7f41a662fbe3254d17cbb231a6fb7808d0287a
Jay <a121406@gmail.com>
2019-04-24 15:34:28 +0800

    change port location

--- src/shape/Shape.java
+++ src/shape/Shape.java
@@ -22,10 +22,10 @@ public class Shape {
         this.xy.setLocation(xy.getX() + dx, xy.getY() + dy);
     }
     public void paintPort(Graphics g){
-        g.fillRect((int)this.xy.getX() + (int)(this.widthHeight.getWidth()/2), (int)this.xy.getY(), PORT_WIDTH, PORT_HEIGHT);
-        g.fillRect((int)this.xy.getX(), (int)(this.xy.getY() + this.widthHeight.getHeight()/2), PORT_WIDTH, PORT_HEIGHT);
-        g.fillRect((int)(this.xy.getX() + this.widthHeight.getWidth()/2), (int)(this.xy.getY() + this.widthHeight.getHeight()), PORT_WIDTH, PORT_HEIGHT);
-        g.fillRect((int)this.xy.getX() + (int)(this.widthHeight.getWidth()), (int)(this.xy.getY() + this.widthHeight.getHeight()/2), PORT_WIDTH, PORT_HEIGHT);
+        g.fillRect((int)this.xy.getX() + (int)(this.widthHeight.getWidth()/2), (int)this.xy.getY()-5, PORT_WIDTH, PORT_HEIGHT);
+        g.fillRect((int)this.xy.getX()-5, (int)(this.xy.getY() + this.widthHeight.getHeight()/2), PORT_WIDTH, PORT_HEIGHT);
+        g.fillRect((int)(this.xy.getX() + this.widthHeight.getWidth()/2), (int)(this.xy.getY() + this.widthHeight.getHeight()-5), PORT_WIDTH, PORT_HEIGHT);
+        g.fillRect((int)this.xy.getX() + (int)(this.widthHeight.getWidth())-5, (int)(this.xy.getY() + this.widthHeight.getHeight()/2), PORT_WIDTH, PORT_HEIGHT);
     }
     public Point[] getPortArray(){
 
